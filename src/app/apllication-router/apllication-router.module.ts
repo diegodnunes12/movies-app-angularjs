5db086d9907e7d4667a65a868f6518e0f3b3ee3d
Diego Dleon Nunes <diegodnunes12@gmail.com>
2020-09-03 19:58:56 -0300

    Criando os detalhes do filme

--- src/app/apllication-router/apllication-router.module.ts
+++ src/app/apllication-router/apllication-router.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { HomeComponent } from '../shared/home/home.component';
+import { AboutComponent } from '../shared/about/about.component';
+import { MovieDetailComponent } from '../movie/movie-detail/movie-detail.component';
 
 const routes: Route[] = [
   {
     path: '', component: HomeComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: 'movie/:title', component: MovieDetailComponent
   }
 ];
 
@@ -14,6 +22,7 @@ const routes: Route[] = [
   imports: [
     CommonModule,
     RouterModule.forRoot(routes)
-  ]
+  ],
+  exports: [RouterModule]
 })
 export class ApllicationRouterModule { }
