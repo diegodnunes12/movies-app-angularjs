4d1538e1d5d1dd74756d60bbb4f3b1a9e9a423d1
Diego Dleon Nunes <diegodnunes12@gmail.com>
2020-09-03 19:26:43 -0300

    Criando estrutura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MovieModule } from './movie/movie.module';
+import { ApllicationRouterModule } from './apllication-router/apllication-router.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MovieModule,
+    ApllicationRouterModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
